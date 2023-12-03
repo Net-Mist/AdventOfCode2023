7c6055b49d5b9b85cf24efb0da7937273f837953
Sebastien Iooss <archimist.linux@gmail.com>
2023-12-03 15:47:10 +0100

    feat: day 3

--- src/day_3.rs
+++ src/day_3.rs
@@ -1,31 +1,141 @@
-pub fn generator(input: &str) -> Vec<i32> {
-    input.lines().map(|n| n.parse().unwrap()).collect()
+use std::collections::{HashMap, HashSet};
+
+type Map = Vec<Vec<u8>>;
+
+const ZERO: u8 = 48;
+const NINE: u8 = 57;
+
+fn is_digit(n: u8) -> bool {
+    n >= ZERO && n <= NINE
 }
 
-pub fn part1(input: &[i32]) -> usize {
-    1
+fn check_valid(x: usize, y: usize, map: &Map) -> bool {
+    for x_d in 0..3 {
+        for y_d in 0..3 {
+            let x = (x + x_d).wrapping_sub(1);
+            let y = (y + y_d).wrapping_sub(1);
+            if x > map.len() - 1 || y > map[0].len() - 1 {
+                continue;
+            }
+            let n = map[x][y];
+            if !is_digit(n) && n != b'.' {
+                return true;
+            }
+        }
+    }
+    return false;
 }
 
-pub fn part2(input: &[i32]) -> usize {
-    1
+fn find_gears(x: usize, y: usize, map: &Map) -> HashSet<(usize, usize)> {
+    let mut out = HashSet::new();
+    for x_d in 0..3 {
+        for y_d in 0..3 {
+            let x = (x + x_d).wrapping_sub(1);
+            let y = (y + y_d).wrapping_sub(1);
+            if x > map.len() - 1 || y > map[0].len() - 1 {
+                continue;
+            }
+            if map[x][y] == b'*' {
+                out.insert((x, y));
+            }
+        }
+    }
+    return out;
+}
+
+pub fn generator(input: &str) -> Vec<Vec<u8>> {
+    input.lines().map(|line| line.bytes().collect()).collect()
+}
+
+pub fn part1(input: &Map) -> u64 {
+    let mut number;
+    let mut is_valid_number;
+    let mut numbers = vec![];
+    for x in 0..input.len() {
+        number = 0;
+        is_valid_number = false;
+        for y in 0..input[0].len() {
+            if is_digit(input[x][y]) {
+                number *= 10;
+                number += (input[x][y] - ZERO) as u64;
+                is_valid_number |= check_valid(x, y, input);
+            } else {
+                if is_valid_number {
+                    numbers.push(number);
+                }
+                number = 0;
+                is_valid_number = false;
+            }
+        }
+        if is_valid_number {
+            numbers.push(number);
+        }
+    }
+    numbers.iter().sum()
+}
+
+pub fn part2(input: &Map) -> u64 {
+    let mut number;
+    let mut gear_to_numbers: HashMap<(usize, usize), Vec<u64>> = HashMap::new();
+    let mut valid_gears;
+    for x in 0..input.len() {
+        number = 0;
+        valid_gears = HashSet::new();
+        for y in 0..input[0].len() {
+            if is_digit(input[x][y]) {
+                number *= 10;
+                number += (input[x][y] - ZERO) as u64;
+                for g in find_gears(x, y, input) {
+                    valid_gears.insert(g);
+                }
+            } else {
+                for g in valid_gears {
+                    if gear_to_numbers.contains_key(&g) {
+                        gear_to_numbers.get_mut(&g).unwrap().push(number);
+                    } else {
+                        gear_to_numbers.insert(g, vec![number]);
+                    }
+                }
+                number = 0;
+                valid_gears = HashSet::new();
+            }
+        }
+
+        for g in valid_gears {
+            if gear_to_numbers.contains_key(&g) {
+                gear_to_numbers.get_mut(&g).unwrap().push(number);
+            } else {
+                gear_to_numbers.insert(g, vec![number]);
+            }
+        }
+    }
+    gear_to_numbers
+        .values()
+        .filter(|v| v.len() == 2)
+        .map(|v| v.iter().product::<u64>())
+        .sum()
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    // use helper_macro::test_parts;
-    // test_parts!(2, 1698735, 1594785890);
+    use aoc_macro::test_parts;
+    test_parts!(3, 507214, 72553319);
 
     #[test]
     fn test_base() {
-        let example = "forward 5\n\
-        down 5\n\
-        forward 8\n\
-        up 3\n\
-        down 8\n\
-        forward 2";
-        assert_eq!(part1(&generator(example)), 150);
-        // assert_eq!(part2(&generator(example)), 900);
+        let example = "467..114..\n\
+            ...*......\n\
+            ..35..633.\n\
+            ......#...\n\
+            617*......\n\
+            .....+.58.\n\
+            ..592.....\n\
+            ......755.\n\
+            ...$.*....\n\
+            .664.598..";
+        assert_eq!(part1(&generator(example)), 4361);
+        assert_eq!(part2(&generator(example)), 467835);
     }
 }
