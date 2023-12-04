e502d4a11c2e61f858eb849b5f4c7769e7124c30
Sebastien Iooss <archimist.linux@gmail.com>
2023-12-04 17:06:18 +0100

    feat: add other way for day 4

--- src/day_4.rs
+++ src/day_4.rs
@@ -1,6 +1,64 @@
-use std::collections::HashSet;
+use ahash::AHashSet;
 
-pub fn generator(input: &str) -> Vec<(HashSet<u16>, HashSet<u16>)> {
+type Data = Vec<(AHashSet<u8>, AHashSet<u8>)>;
+
+pub fn generator(input: &str) -> &str {
+    input
+}
+
+pub fn generator_bitset(input: &str) -> Vec<u32> {
+    // number are all lower than 100. So instead of using a hashing function,
+    // we could set bit of a u128 to 1 if the number is present, and compute the
+    // intersection with a logical or
+    input
+        .lines()
+        .map(|line| {
+            let (_, line) = line.split_once(": ").unwrap();
+            let (wining, numbers) = line.split_once(" | ").unwrap();
+            let wining = wining
+                .split(' ')
+                .filter(|n| !n.is_empty())
+                .map(|n| 1 << n.parse::<u8>().unwrap())
+                .reduce(|acc: u128, e| acc | e)
+                .unwrap();
+            let numbers = numbers
+                .split(' ')
+                .filter(|n| !n.is_empty())
+                .map(|n| 1 << n.parse::<u8>().unwrap())
+                .reduce(|acc: u128, e| acc | e)
+                .unwrap();
+            let r: u128 = wining & numbers;
+            r.count_ones()
+        })
+        .collect()
+}
+
+pub fn part1(input: &str) -> usize {
+    generator_bitset(input)
+        .iter()
+        .map(|i| {
+            if *i == 0 {
+                return 0;
+            }
+            1 << (*i - 1)
+        })
+        .sum()
+}
+
+pub fn part2(input: &str) -> usize {
+    let input = generator_bitset(input);
+    let mut weight_vector = vec![1; input.len()];
+    for (i, n) in input.iter().enumerate() {
+        for j in 0..*n as usize {
+            if i + j + 1 < weight_vector.len() {
+                weight_vector[i + j + 1] += weight_vector[i];
+            }
+        }
+    }
+    weight_vector.iter().sum()
+}
+
+pub fn generator_set(input: &str) -> Data {
     input
         .lines()
         .map(|line| {
@@ -21,8 +79,8 @@ pub fn generator(input: &str) -> Vec<(HashSet<u16>, HashSet<u16>)> {
         .collect()
 }
 
-pub fn part1(input: &Vec<(HashSet<u16>, HashSet<u16>)>) -> usize {
-    input
+pub fn part1_set(input: &str) -> usize {
+    generator_set(input)
         .iter()
         .map(|(w, n)| {
             let i = w.intersection(n).count();
@@ -34,7 +92,8 @@ pub fn part1(input: &Vec<(HashSet<u16>, HashSet<u16>)>) -> usize {
         .sum()
 }
 
-pub fn part2(input: &Vec<(HashSet<u16>, HashSet<u16>)>) -> usize {
+pub fn part2_set(input: &str) -> usize {
+    let input = generator_set(input);
     let mut weight_vector = vec![1; input.len()];
     for (i, (w, n)) in input.iter().enumerate() {
         for j in 0..w.intersection(n).count() {
