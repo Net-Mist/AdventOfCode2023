2869f72b87ad1a6498d8f1e2ed55e95746f5bb76
Sebastien Iooss <archimist.linux@gmail.com>
2023-12-11 09:17:43 +0100

    fix: better day 8

--- src/day_8.rs
+++ src/day_8.rs
@@ -1,4 +1,5 @@
-use nohash_hasher::NoHashHasher;
+use arrayvec::ArrayVec;
+use nohash_hasher::{BuildNoHashHasher, NoHashHasher};
 use std::{collections::HashMap, hash::BuildHasherDefault};
 
 use num::integer::lcm;
@@ -6,8 +7,41 @@ use num::integer::lcm;
 const MIN_ASCII: u8 = 48;
 const RANGE_ASCII: u32 = 90 - 48;
 
-type Map = HashMap<u32, (u32, u32), BuildHasherDefault<NoHashHasher<u32>>>;
-type Type = (Vec<u8>, Map);
+type Map = HashMap<u32, u16, BuildHasherDefault<NoHashHasher<u32>>>;
+type Type = (Vec<u8>, Maps);
+
+type Int = u16;
+
+#[derive(Default, Debug)]
+struct Node {
+    left: Int,
+    right: Int,
+}
+
+#[derive(Default, Debug)]
+pub struct Maps {
+    nodes: ArrayVec<Node, 766>,
+    begining_part2: ArrayVec<Int, 10>,
+    end_part2: ArrayVec<Int, 10>,
+    beginning_part1: Int,
+    end_part1: Int,
+}
+
+impl Maps {
+    fn left_node(&self, id: Int) -> Int {
+        self.nodes[id as usize].left
+    }
+
+    fn right_node(&self, id: Int) -> Int {
+        self.nodes[id as usize].right
+    }
+
+    fn new_node(&mut self) -> Int {
+        let id = self.nodes.len() as Int;
+        self.nodes.push(Node::default());
+        id
+    }
+}
 
 fn perfect_hash(text: &str) -> u32 {
     text.bytes()
@@ -18,32 +52,58 @@ fn perfect_hash(text: &str) -> u32 {
 
 pub fn generator(input: &str) -> Type {
     let (directions, maps) = input.split_once("\n\n").unwrap();
-    let maps = maps
-        .lines()
-        .map(|l| {
-            let (map, links) = l.split_once(" = ").unwrap();
-            let links = links[1..links.len() - 1].split_once(", ").unwrap();
-            (
-                perfect_hash(map),
-                (perfect_hash(links.0), perfect_hash(links.1)),
-            )
-        })
-        .collect();
-    (directions.bytes().collect(), maps)
+
+    let mut str_to_id: Map = HashMap::with_capacity_and_hasher(766, BuildNoHashHasher::default());
+    let mut all_maps = Maps::default();
+
+    for l in maps.lines() {
+        let (map, links) = l.split_once(" = ").unwrap();
+        let links = links[1..links.len() - 1].split_once(", ").unwrap();
+
+        let map_id = str_to_id
+            .entry(perfect_hash(map))
+            .or_insert_with(|| all_maps.new_node())
+            .to_owned();
+        let left_id = str_to_id
+            .entry(perfect_hash(links.0))
+            .or_insert_with(|| all_maps.new_node())
+            .to_owned();
+        let right_id = str_to_id
+            .entry(perfect_hash(links.1))
+            .or_insert_with(|| all_maps.new_node())
+            .to_owned();
+        all_maps.nodes[map_id as usize] = Node {
+            left: left_id.to_owned() as u16,
+            right: right_id.to_owned() as u16,
+        };
+
+        if map == "AAA" {
+            all_maps.beginning_part1 = map_id;
+        }
+        if map == "ZZZ" {
+            all_maps.end_part1 = map_id;
+        }
+        if map.ends_with("A") {
+            all_maps.begining_part2.push(map_id);
+        }
+        if map.ends_with("Z") {
+            all_maps.end_part2.push(map_id);
+        }
+    }
+    (directions.bytes().collect(), all_maps)
 }
 
 pub fn part1(input: &Type) -> usize {
     let directions = &input.0;
     let maps = &input.1;
-    let mut current_map = perfect_hash("AAA");
+    let mut current_map = maps.beginning_part1;
 
     let mut i = 0;
-    while current_map != perfect_hash("ZZZ") {
-        let m = maps.get(&current_map).unwrap();
+    while current_map != maps.end_part1 {
         if directions[i % directions.len()] == b'R' {
-            current_map = m.1;
+            current_map = maps.right_node(current_map);
         } else {
-            current_map = m.0;
+            current_map = maps.left_node(current_map);
         }
         i += 1;
     }
@@ -54,22 +114,21 @@ pub fn part2(input: &Type) -> usize {
     let directions = &input.0;
     let maps = &input.1;
 
-    maps.keys()
-        .filter(|v| **v % RANGE_ASCII == (b'A' - MIN_ASCII) as u32)
+    maps.begining_part2
+        .iter()
         .map(|v| compute_n(*v, maps, directions))
         .reduce(lcm)
         .unwrap()
 }
 
-fn compute_n(current_map: u32, maps: &Map, directions: &Vec<u8>) -> usize {
+fn compute_n(current_map: Int, maps: &Maps, directions: &Vec<u8>) -> usize {
     let mut i = 0;
     let mut current_map = current_map;
-    while 0 != (current_map % RANGE_ASCII) as u8 {
-        let m = maps.get(&current_map).unwrap();
+    while !maps.end_part2.contains(&current_map) {
         if directions[i % directions.len()] == b'R' {
-            current_map = m.1;
+            current_map = maps.right_node(current_map);
         } else {
-            current_map = m.0;
+            current_map = maps.left_node(current_map);
         }
         i += 1;
     }
