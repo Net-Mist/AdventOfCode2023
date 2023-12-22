3ae2352b6f8355ed948d5f77ed7cf5405511b7cd
Sebastien Iooss <archimist.linux@gmail.com>
2023-12-22 11:57:50 +0100

    feat: day 22

--- src/day_22.rs
+++ src/day_22.rs
@@ -1,29 +1,120 @@
-use std::str::from_utf8;
+use std::{collections::VecDeque, str::from_utf8};
 
-pub fn generator(input: &[u8]) -> &str {
-    let input = from_utf8(input).unwrap();
-    input
+use ahash::{HashSet, HashSetExt};
+
+type Input = Vec<Node>;
+
+#[derive(Debug)]
+pub struct Node {
+    supported_by: HashSet<u16>,
+    support: HashSet<u16>,
 }
 
-pub fn part1(_input: &str) -> usize {
-    1
+pub fn generator(input: &[u8]) -> Input {
+    // order bricks by increasing Z
+    // Fill an array of size 10*10*360 (36K u16)
+    let mut map = vec![vec![vec![0; 370]; 10]; 10];
+    let mut blocks: Vec<Vec<u16>> = input[0..input.len() - 1]
+        .split(|b| b == &b'\n')
+        .map(|line| {
+            line.split(|b| b == &b',' || b == &b'~')
+                .map(|n| from_utf8(n).unwrap().parse::<u16>().unwrap())
+                .collect()
+        })
+        .collect();
+    blocks.sort_unstable_by(|a, b| a[2].partial_cmp(&b[2]).unwrap());
+
+    let mut graph: Vec<Node> = Vec::with_capacity(blocks.len());
+
+    for (block_id, block) in blocks.into_iter().enumerate() {
+        let mut delta = 1usize;
+        let mut supported_by = HashSet::with_capacity(10);
+        while block[2] as usize > delta {
+            for i in block[0]..=block[3] {
+                for j in block[1]..=block[4] {
+                    for k in block[2]..=block[5] {
+                        let v = map[i as usize][j as usize][k as usize - delta];
+                        if v != 0 {
+                            supported_by.insert((v - 1) as u16);
+                        }
+                    }
+                }
+            }
+            if !supported_by.is_empty() {
+                break;
+            }
+            delta += 1;
+        }
+
+        for i in block[0]..=block[3] {
+            for j in block[1]..=block[4] {
+                for k in block[2]..=block[5] {
+                    map[i as usize][j as usize][k as usize + 1 - delta] = block_id + 1;
+                }
+            }
+        }
+        for id in supported_by.iter() {
+            graph[*id as usize].support.insert(block_id as u16);
+        }
+        graph.push(Node {
+            supported_by,
+            support: HashSet::new(),
+        });
+    }
+    graph
 }
 
-pub fn part2(_input: &str) -> usize {
-    1
+pub fn part1(graph: &Input) -> usize {
+    graph
+        .iter()
+        .filter(|node| {
+            node.support
+                .iter()
+                .all(|node| graph[*node as usize].supported_by.len() != 1)
+        })
+        .count()
+}
+
+pub fn part2(input: &Input) -> usize {
+    let mut s = 0;
+    for i in 0..input.len() {
+        let mut removed_id = HashSet::new();
+        let mut removed = VecDeque::new();
+        removed.push_back(i as u16);
+        removed_id.insert(i as u16);
+        while let Some(i) = removed.pop_front() {
+            let node = &input[i as usize];
+            for node_id in node.support.iter() {
+                let node = &input[*node_id as usize];
+                if node.supported_by.difference(&removed_id).count() == 0 {
+                    removed_id.insert(*node_id as u16);
+                    removed.push_back(*node_id as u16);
+                }
+            }
+        }
+        s += removed_id.len() - 1;
+    }
+    s
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
-    // use aoc_macro::test_parts;
-    // test_parts!(2, 1698735, 1594785890);
+    use aoc_macro::test_parts;
+    test_parts!(22, 492, 86556);
 
     #[test]
     fn test_base() {
-        let example = "\n".as_bytes();
-        assert_eq!(part1(generator(example)), 1);
-        assert_eq!(part2(generator(example)), 1);
+        let example = "1,0,1~1,2,1\n\
+                                0,0,2~2,0,2\n\
+                                0,2,3~2,2,3\n\
+                                0,0,4~0,2,4\n\
+                                2,0,5~2,2,5\n\
+                                0,1,6~2,1,6\n\
+                               1,1,8~1,1,9\n"
+            .as_bytes();
+        assert_eq!(part1(&generator(example)), 5);
+        assert_eq!(part2(&generator(example)), 7);
     }
 }
