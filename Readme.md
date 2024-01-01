ed04df9ef30c99d7776350532cd68dc4f984af46
Sebastien Iooss <archimist.linux@gmail.com>
2024-01-01 12:33:34 +0100

    fix: faster day16

--- Readme.md
+++ Readme.md
@@ -6,10 +6,11 @@ I tried to have the fastest solution possible by implementing concepts from [The
 
 ## Things learn
 
-- When size of vector are known, use ArrayVec to prevent Heap allocation
-- at the scale of AoC problems, hashes are slow. Even ahash or noHash. Prefer using bitwise operations if possible
+- When size of vector are known, use ArrayVec to prevent Heap allocation.
+- at the scale of AoC problems, hashes are slow. Even ahash or noHash. Prefer using bitwise operations if possible.
+- Created nested vect, like `vec![vec![false; W]; H]` can be slow because of memory allocations. Prefer `vec![false; W * H]` or bitwise version like `vec![0u64; (W * H + 63)/64]` (`+63` is for rounding up).
 - `cargo-show-asm` is great to investigate ASM code. A function can be marked as `#[inline(never)]` if it doesn't appear in the assembly.
-- [Shoelace formula](https://en.wikipedia.org/wiki/Shoelace_formula) and [Pick's theorem](https://en.wikipedia.org/wiki/Pick%27s_theorem) are great tools when computing area of big polygons
+- [Shoelace formula](https://en.wikipedia.org/wiki/Shoelace_formula) and [Pick's theorem](https://en.wikipedia.org/wiki/Pick%27s_theorem) are great tools when computing area of big polygons.
 
 ## Profiling
 
--- src/day_16.rs
+++ src/day_16.rs
@@ -1,7 +1,5 @@
 use std::str::from_utf8;
 
-use ahash::{HashSet, HashSetExt};
-
 pub fn generator(input: &[u8]) -> Vec<&[u8]> {
     let input = from_utf8(input).unwrap();
 
@@ -17,30 +15,60 @@ pub fn part1(input: &[&[u8]]) -> usize {
     find_n_energized(initial_position, initial_direction, input)
 }
 
+fn direction_to_index(direction: &(i32, i32)) -> usize {
+    match direction {
+        (-1, 0) => 0,
+        (1, 0) => 1,
+        (0, -1) => 2,
+        (0, 1) => 3,
+        _ => unreachable!(),
+    }
+}
+
 fn find_n_energized(
     initial_position: (usize, usize),
     initial_direction: (i32, i32),
     input: &[&[u8]],
 ) -> usize {
-    let mut to_process = vec![(initial_position, initial_direction)];
-    let h = input.len();
-    let w = input[0].len();
-    let mut energized = vec![vec![false; w]; h];
-    let mut seen = HashSet::new();
-    while let Some((position, direction)) = to_process.pop() {
+    // let mut to_process = vec![(initial_position, initial_direction)];
+    let mut to_process = Vec::with_capacity(50);
+    to_process.push((initial_position, initial_direction));
+    let h = input.len(); // 110
+    let w = input[0].len(); // 110
+
+    // keep track of energized cells
+    let mut energized = vec![0u64; (w * h + 63) / 64];
+
+    // keep track of seen direction for each energized cells
+    let mut seen_direction = vec![0u64; (4 * w * h + 63) / 64];
+    'main: while let Some((mut position, direction)) = to_process.pop() {
         if position.0 >= h || position.1 >= w {
             continue;
         }
-        if seen.contains(&(position, direction)) {
+        let seen_direction_id = (position.0 * w + position.1) * 4 + direction_to_index(&direction);
+        let energized_id = position.0 * w + position.1;
+        if seen_direction[seen_direction_id / 64] >> (seen_direction_id % 64) & 1 == 1 {
             continue;
         }
-        seen.insert((position, direction));
-        energized[position.0][position.1] = true;
-        let t = input.get(position.0).unwrap().get(position.1).unwrap();
+        seen_direction[seen_direction_id / 64] |= 1 << (seen_direction_id % 64);
+        energized[energized_id / 64] |= 1 << (energized_id % 64);
+        let mut t = input.get(position.0).unwrap().get(position.1).unwrap();
+
+        let udirection = (direction.0 as usize, direction.1 as usize);
+        while t == &b'.' {
+            position.0 = position.0.wrapping_add(udirection.0);
+            position.1 = position.1.wrapping_add(udirection.1);
+            if position.0 >= h || position.1 >= w {
+                continue 'main;
+            }
+            let energized_id = position.0 * w + position.1;
+            energized[energized_id / 64] |= 1 << (energized_id % 64);
+            t = input.get(position.0).unwrap().get(position.1).unwrap();
+        }
         match direction {
             // right
             (0, 1) => match t {
-                b'-' | b'.' => {
+                b'-' => {
                     to_process.push(((position.0, position.1 + 1), direction));
                 }
                 b'|' => {
@@ -57,7 +85,7 @@ fn find_n_energized(
             },
             // left
             (0, -1) => match t {
-                b'-' | b'.' => {
+                b'-' => {
                     to_process.push(((position.0, position.1.wrapping_sub(1)), direction));
                 }
                 b'|' => {
@@ -74,7 +102,7 @@ fn find_n_energized(
             },
             // down
             (1, 0) => match t {
-                b'|' | b'.' => {
+                b'|' => {
                     to_process.push(((position.0 + 1, position.1), direction));
                 }
                 b'-' => {
@@ -91,7 +119,7 @@ fn find_n_energized(
             },
             // up
             (-1, 0) => match t {
-                b'|' | b'.' => {
+                b'|' => {
                     to_process.push(((position.0.wrapping_sub(1), position.1), direction));
                 }
                 b'-' => {
@@ -109,10 +137,7 @@ fn find_n_energized(
             _ => unreachable!(),
         }
     }
-    energized
-        .iter()
-        .map(|line| line.iter().filter(|e| **e).count())
-        .sum()
+    energized.into_iter().map(|b| b.count_ones() as usize).sum()
 }
 
 pub fn part2(input: &[&[u8]]) -> usize {
